8991885fee64f929a74cef6b8df815371011a489
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-11-04 16:35:54 +0000

    Goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -17,52 +17,66 @@ const hhconfig: HardhatUserConfig = {
     },
   },
   networks: {
-    mainnet: {
-      url: process.env.ETHEREUM_RPC_URL,
+    // mainnet: {
+    //   url: process.env.ETHEREUM_RPC_URL,
+    //   accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+    //   gasPrice: 9000000000, // this is 30 Gwei
+    // },
+    goerli: {
+      url: process.env.GOERLI_RPC_URL,
       accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
-      gasPrice: 9000000000, // this is 30 Gwei
+      gasPrice: 'auto',
     },
-    polygon: {
-      url: process.env.POLYGON_RPC_URL,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
-      gasPrice: 90000000000, // this is 30 Gwei
-    },
-    base: {
-      url: process.env.BASE_RPC_URL,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
-      gasPrice: 30000000, // this is 30 Gwei
-    },
-    celo: {
-      url: process.env.CELO_RPC_URL,
-      accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
-      gasPrice: 30000000, // this is 30 Gwei
-    },
+    // polygon: {
+    //   url: process.env.POLYGON_RPC_URL,
+    //   accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+    //   gasPrice: 90000000000, // this is 30 Gwei
+    // },
+    // base: {
+    //   url: process.env.BASE_RPC_URL,
+    //   accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+    //   gasPrice: 30000000, // this is 30 Gwei
+    // },
+    // celo: {
+    //   url: process.env.CELO_RPC_URL,
+    //   accounts: [process.env.DEPLOYER_PRIVATE_KEY || "0x0"],
+    //   gasPrice: 30000000, // this is 30 Gwei
+    // },
   },
   etherscan: {
     apiKey: {
-      mainnet: process.env.ETEHREUM_MAINNET_ETHERSCAN_API_KEY || "",
-      polygonMumbai: process.env.POLYGON_MUMBAI_ETHERSCAN_API_KEY || "",
-      polygon: process.env.POLYGON_MAINNET_ETHERSCAN_API_KEY || "",
-      base: process.env.BASE_MAINNET_ETHERSCAN_API_KEY || "",
-      celo: process.env.CELO_MAINNET_ETHERSCAN_API_KEY || "",
+      // mainnet: process.env.ETEHREUM_MAINNET_ETHERSCAN_API_KEY || "",
+      goerli: process.env.ETHEREUM_GOERLI_ETHERSCAN_API_KEY || "",
+      // polygonMumbai: process.env.POLYGON_MUMBAI_ETHERSCAN_API_KEY || "",
+      // polygon: process.env.POLYGON_MAINNET_ETHERSCAN_API_KEY || "",
+      // base: process.env.BASE_MAINNET_ETHERSCAN_API_KEY || "",
+      // celo: process.env.CELO_MAINNET_ETHERSCAN_API_KEY || "",
     },
     customChains: [
       {
-        network: "base",
-        chainId: 8453,
+        network: "goerli",
+        chainId: 5,
         urls: {
-          apiURL: "https://api.basescan.org/api",
-          browserURL: "https://basescan.org",
-        },
+          apiURL: "https://api-goerli.etherscan.io/api",
+          browserURL: "https://goerli.etherscan.io"
+        }
       },
-      {
-        network: "celo",
-        chainId: 42220,
-        urls: {
-          apiURL: "https://api.celoscan.io",
-          browserURL: "https://celoscan.io",
-        },
-      },
+      // {
+      //   network: "base",
+      //   chainId: 8453,
+      //   urls: {
+      //     apiURL: "https://api.basescan.org/api",
+      //     browserURL: "https://basescan.org",
+      //   },
+      // },
+      // {
+      //   network: "celo",
+      //   chainId: 42220,
+      //   urls: {
+      //     apiURL: "https://api.celoscan.io",
+      //     browserURL: "https://celoscan.io",
+      //   },
+      // },
     ],
   },
 };
